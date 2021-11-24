fedd9b21b4b87e559debcbf334cf87ffe592966d
Chris Lemke <cwlemke@live.com>
2021-11-23 16:31:06 -0800

    Initial springboot/mvn api

---
+++ api/src/main/java/choiceawards/api/ChoiceAwardsApiApplication.java
@@ -0,0 +1,13 @@
+package choiceawards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceAwardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiceAwardsApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/choiceawards/api/ChoiceAwardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package choiceawards.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceAwardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
