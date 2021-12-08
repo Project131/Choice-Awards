5f4f5baf9b06aea9d842c61b8e288d4b8130ac8a
williams4564 <williams4564@gmail.com>
2021-12-07 16:24:37 -0800

    added test code for parsing url queries to jpa DB

--- api/src/main/java/choiceawards/api/ApiApplication.java
+++ api/src/main/java/choiceawards/api/ApiApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+
+import java.util.*;
+import org.springframework.web.bind.annotation.RequestMapping;  
+
 @SpringBootApplication
 public class ApiApplication {
 
@@ -18,16 +22,50 @@ public class ApiApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-
+					
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
+		/*	String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println(beanName);
 			}
+			*/
+			
 
 		};
 	}
+	//how to pass path paramteter in url in spring rest
+	@RequestMapping(method = RequestMethod.GET, value = "/custom")
+    public String controllerMethod(@RequestParam Map<String, String> customQuery) {
+
+        System.out.println("customQuery = brand " + customQuery.containsKey("brand"));
+        System.out.println("customQuery = limit " + customQuery.containsKey("limit"));
+        System.out.println("customQuery = price " + customQuery.containsKey("price"));
+        System.out.println("customQuery = other " + customQuery.containsKey("other"));
+        System.out.println("customQuery = sort " + customQuery.containsKey("sort"));
+
+        return customQuery.toString();
+    }
+	//calling query:
+	@Query(value="SELECT *FROM movies WHERE categories=?bestpicture". nativeQuery=true);
+	List<Movie> getMovieByCategory(String category);
+	public List<Movie> getMovieByCategory(String category){
+		List <Movie> catList = //query DB
+		catList.forEach(System.out::println);
+	}
+	//get url like this, test
+	@GetMapping(path = {"/user", "/user/{data}"})
+	public void user(@PathVariable(required=false,name="data") String data, @RequestParam(required=false) Map<String,String> qparams) {
+		qparams.forEach((a,b) -> {
+			System.out.println(String.format("%s -> %s",a,b));
+		};
+	
+		if (data != null) {
+			System.out.println(data);
+		}
+		
+	};
+	
 
 }
